cc7cae2cd366118af8b34ee383adc91af031fc8f
De1v <87398538+PaN-De1v@users.noreply.github.com>
2022-11-29 14:34:07 +0100

    Update Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,7 +3,24 @@ import React from "react";
 function Projects() {
   return(
     <div>
-      <p>PaN_De1v P</p>
+      <nav className="bg-[#171a18] p-5 nav">
+        <ul className="inline-flex gap-5">
+          <li>
+            <Link to="/" className="hover:text-[#20FD6A] transition duration-300">Home</Link>
+          </li>
+          <li>
+            <Link to="/knowledge" className="hover:text-[#20FD6A] transition duration-300">Knowledge</Link>
+          </li>
+          <li>
+            <Link to="/projects" className="hover:text-[#20FD6A] transition duration-300">Projects</Link>
+          </li>
+        </ul>
+      </nav>
+    
+      <div className="container">
+        <div className="bg-[#171a15]">
+        </div>
+      </div>
     </div>
   )
 }
